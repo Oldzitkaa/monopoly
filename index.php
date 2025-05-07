87a9bff68560b0e7f57dee8183570529dbaf6d86
Oldzitkaa <olgunia.spychala@gmail.com>
2025-05-07 12:17:37 +0200

    przylaczona baza i zmiana rozszerzenia pliku

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once './database_connect.php';?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -7,12 +8,14 @@
     <link rel="stylesheet" href="./css/styl.css">
 </head>
 <body>
+<!-- div pierwszy -->
     <div class="logo_div step1">
         <p class="monopoly size1">MONOPOLY</p>
         <img src="./zdj/logo.png" alt="Potega Smakow" class="logo_zdj">
         <button class="next nextright" onclick="one()"><p class="nextstep size3">	&larr;</p></button>
       </div>
       
+<!-- div drugi -->
       <div class="logo_div step2" >
         <!-- style="display: none; opacity: 0;" -->
         <p class="monopoly size1">Podaj liczbe <br> graczy</p><br>
@@ -21,15 +24,10 @@
             <input type="range" class=" rangeplayer" name="quantity" id="quantity" min="2" max="4" value="2">
         </label>
         <button class="next nextleft" onclick="two()"><p class="nextstep size3">	&rarr;</p></button>
-        <!-- <div class="player">
-          <label for=""><p>Gracz 1</p><input type="text" name="" id=""></label>
-        </div>
-        <div class="player">
-          <label for=""><p>Gracz 2</p><input type="text" name="" id=""></label>
-        </div> -->
       </div>
 
-      <div class="logo_div step3" >
+<!-- div trzeci -->
+      <!-- <div class="logo_div step3" >
         <p class="monopoly size1">Podaj niki <br> graczy</p><br>
         <p id="playerCount" class="size2">2</p>
         
@@ -44,7 +42,7 @@
       
       <img src="./zdj/tlo3.png" alt="" class="backg">
       
-      <script src="./js/script_main.js"></script>
+      <script src="./js/script_main.js"></script> -->
 </body>
 </html>
 <!-- vw mixin
